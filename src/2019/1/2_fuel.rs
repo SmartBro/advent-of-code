f571173fdf23721866b2f0204092dbb94c90da32
SmartBro <smartbrostudio@gmail.com>
2019-12-13 23:08:52 +0200

    day 1 refactor

--- src/2019/1/2_fuel.rs
+++ src/2019/1/2_fuel.rs
@@ -9,12 +9,16 @@ fn main() -> std::io::Result<()> {
     let mut fuel_required = 0;
     for line in contents.lines() {
         let mut input: i32 = line.parse().unwrap();
-        input = input / 3 - 2;
+        input = calc_fuel(input);
         while input > 0 {
             fuel_required += input;
-            input = input / 3 - 2;
+            input = calc_fuel(input);
         }
     }
     println!("Fuel Required: {}", fuel_required);
     Ok(())
+}
+
+fn calc_fuel(input: i32) -> i32 {
+    input / 3 - 2
 }
